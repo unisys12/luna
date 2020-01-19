123b43742626187824900f1faeffcc033db2c4b8
Phillip Jackson <unisys12@gmail.com>
2020-01-19 11:08:41 -0600

    removed unused callouts

--- front-page.php
+++ front-page.php
@@ -9,31 +9,13 @@
                 <img src="https://res.cloudinary.com/dtm8qhbwk/image/upload/v1573864836/LeadMeHome_Logo_Cropped_fsvomx.png" alt="Lead Me Home Logo" class="luna_img" width="409" height="512">
             </figure>
             <div class="action">
-                <h2 class="action-header">Adobt Your Next Pet!</h2>
+                <h2 class="action-header">Rescue Your Next Pet!</h2>
                 <a href="/dogs" class="adoption-btn">For Luna...</a>
             </div>
         </div>
-        <div class="callouts">
-            <div class="card">
-                <h2>Callout</h2>
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error odio tempora, neque a minima dicta sed cumque amet excepturi ullam, maiores itaque, vitae placeat quaerat? Repellat accusamus facilis nam.</p>
-                </div>
-            </div>
-            <div class="card">
-                <h2>Callout</h2>
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error odio tempora, neque a minima dicta sed cumque amet excepturi ullam, maiores itaque, vitae placeat quaerat? Repellat accusamus facilis nam.</p>
-                </div>
-            </div>
-            <div class="card">
-                <h2>Callout</h2>
-                <div class="content">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error odio tempora, neque a minima dicta sed cumque amet excepturi ullam, maiores itaque, vitae placeat quaerat? Repellat accusamus facilis nam.</p>
-                </div>
-            </div>
-        </div>
+
     </main>
 
     <?php get_footer(); ?>
+</div>
 </div>

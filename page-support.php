f4e4d6e3400d4c2fb72bf7ef0ddfd8c370d2afea
Phillip Jackson <unisys12@gmail.com>
2020-02-16 08:13:14 -0600

    add support page and styling

---
+++ page-support.php
@@ -0,0 +1,8 @@
+<?php require_once "partials/site_head.php"; ?>
+<div class="wrapper">
+    <?php get_header(); ?>
+
+    <?php get_template_part('partials/section', 'support-grid'); ?>
+
+    <?php get_footer(); ?>
+</div>
---
+++ partials/section-support-grid.php
@@ -0,0 +1,31 @@
+<main class="containter">
+    <h2 class="page-head">
+        Ways to Support Us
+    </h2>
+    <section class="support-grid">
+        <article class="payment-card">
+            <h3>PayPal</h3>
+            <figure class="payment-logo">
+                <img src="https://res.cloudinary.com/dtm8qhbwk/image/upload/v1581512359/paypal-logo-preview_fbsps4.png" height="auto" width="150px" alt="paypal logo">
+            </figure>
+        </article>
+        <article class="payment-card">
+            <h3>Cash App</h3>
+            <figure class="payment-logo">
+                <img src="https://res.cloudinary.com/dtm8qhbwk/image/upload/v1581512343/Cash-App-Dollar-Icon_svycpe.png" height="auto" width="150px" alt="cash app logo">
+            </figure>
+        </article>
+        <article class="payment-card">
+            <h3>Venmo</h3>
+            <figure class="payment-logo">
+                <img src="https://res.cloudinary.com/dtm8qhbwk/image/upload/v1581597010/venmo-icon_l5wpmj.svg" height="auto" width="150px" alt="Venmo logo">
+            </figure>
+        </article>
+        <article class="payment-card">
+            <h3>Apple Pay</h3>
+            <figure class="payment-logo">
+                <img src="https://res.cloudinary.com/dtm8qhbwk/image/upload/v1581597239/Apple-Pay-logo-vector-01_kwwbqx.svg" height="auto" width="150px" alt="Apple Pay logo">
+            </figure>
+        </article>
+    </section>
+</main>
